61dea4b8c17266c84f6ad241530abe426c45ee6a
seahpark247 <famousjedo@gmail.com>
2025-04-19 20:39:03 -0500

    Initial Commit

---
+++ GoalChaser/GoalChaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalChaserApp.swift
+//  GoalChaser
+//
+//  Created by Seah Park on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct GoalChaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoalChaserTests/GoalChaserTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoalChaserTests.swift
+//  GoalChaserTests
+//
+//  Created by Seah Park on 4/19/25.
+//
+
+import Testing
+@testable import GoalChaser
+
+struct GoalChaserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
